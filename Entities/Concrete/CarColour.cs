fd3b2bb73e32bdabe51863ce392d1caf97b7b269
Gülnisa Aslan <gulnisaslan@hotmail.com>
2021-01-31 22:15:35 +0300

    Add project files.

---
+++ Entities/Concrete/CarColour.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public  class CarColour:IEntity
+    {
+        public int ColourId { get; set; }
+        public string  ColourName { get; set; }
+    }
+}
